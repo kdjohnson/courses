c8933b49628da22cc29d84e79b548e2859a00c99
KaJuan Johnson <kdjohnson23@oakland.edu>
2017-05-26 13:24:32 -0400

    Add ability to change terms #3

--- src/fetchData.js
+++ src/fetchData.js
@@ -1,19 +1,35 @@
-export const getTerms = () => {
-  return fetch("http://localhost:8082/api/terms")
-    .then(response => {
-      return response.json()
-    })
-    .then(myJSON => {
-      return myJSON.terms
-    })
-    .catch(err => {
-      return err
-    })
+export const getTerms = async () => {
+  try {
+    const response = await fetch("http://localhost:8082/api/terms")
+    const terms = await response.json()
+    return terms.terms
+  } catch (err) {
+    return err
+  }
 }
 
-export const getCourses = () => {
+export const getCourses = async code => {
+  try {
+    console.log("getCourses")
+    console.log(code)
+    const response = await fetch("http://localhost:8082/api/courses", {
+      body: JSON.stringify({ code: code }),
+      method: "POST"
+    })
+    const courses = await response.json()
+    return courses.courses
+  } catch (err) {
+    console.error(err)
+    return err
+  }
+}
+
+/*
+export const getCourses = code => {
+  console.log("getCourses")
+  console.log(code)
   return fetch("http://localhost:8082/api/courses", {
-    body: JSON.stringify({ code: "201830" }),
+    body: JSON.stringify({ code: code }),
     method: "POST"
   })
     .then(response => {
@@ -26,3 +42,4 @@ export const getCourses = () => {
       return err
     })
 }
+*/
