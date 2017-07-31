b214cb28369c4cb9664d45310c5233392d0650bd
KaJuan Johnson <kdjohnson23@oakland.edu>
2017-07-31 11:38:59 -0400

    Use error icon #85

--- src/components/ErrorMessages.js
+++ src/components/ErrorMessages.js
@@ -9,6 +9,7 @@ import Card, {
 import { withStyles, createStyleSheet } from "material-ui/styles"
 import PropTypes from "prop-types"
 import grizzHead from "../img/grizzHead.png"
+import Error from "material-ui-icons/Error"
 import Button from "material-ui/Button"
 
 const styleSheet = createStyleSheet("ErrorMessages", theme => ({
@@ -17,28 +18,39 @@ const styleSheet = createStyleSheet("ErrorMessages", theme => ({
     maxWidth: "650px",
     textAlign: "ltr"
   },
+
   card: {
     backgroundColor: "#fafafa ",
     borderLeftStyle: "solid",
     borderLeftWidth: "12px",
     borderLeftColor: "#d32f2f"
   },
+
   media: {
     padding: "10px",
     display: "flex",
     alignSelf: "center",
     marginLeft: "10px"
   },
+
   main: {
     display: "flex",
     flexDirection: "row"
   },
+
   head: {
     display: "flex",
     flexFlow: "column wrap"
   },
+
   content: {
     paddingTop: 0
+  },
+
+  icon: {
+    fill: "#d32f2f",
+    width: 75,
+    height: 100
   }
 }))
 
@@ -50,13 +62,7 @@ class ErrorMessages extends Component {
         <Card className={classes.card}>
           <div className={classes.main}>
             <CardMedia className={classes.media}>
-              <img
-                src={grizzHead}
-                width="123.6px"
-                height="95.6px"
-                alt="Grizz Head Image"
-                aria-hidden="true"
-              />
+              <Error className={classes.icon} />
             </CardMedia>
             <div className={classes.head}>
               <CardHeader title="We were unable to fetch data at this time" />
