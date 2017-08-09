9c413b3e90eb2d058eba32d4daf4334c358dc529
KaJuan Johnson <kdjohnson23@oakland.edu>
2017-08-09 15:56:08 -0400

    Remove card actions #119

--- src/components/ErrorMessages.js
+++ src/components/ErrorMessages.js
@@ -1,28 +1,22 @@
 import React, { Component } from "react"
 import Typography from "material-ui/Typography"
-import Card, {
-  CardHeader,
-  CardContent,
-  CardMedia,
-  CardActions
-} from "material-ui/Card"
+import Card, { CardHeader, CardContent, CardMedia } from "material-ui/Card"
 import { withStyles, createStyleSheet } from "material-ui/styles"
 import PropTypes from "prop-types"
 import Error from "material-ui-icons/Error"
-import Button from "material-ui/Button"
 
 const styleSheet = createStyleSheet("ErrorMessages", theme => ({
   root: {
     position: "relative",
-    width: "100%",
-    textAlign: "ltr"
+    width: "90%"
   },
 
   card: {
     backgroundColor: "#fafafa ",
     borderLeftStyle: "solid",
     borderLeftWidth: "12px",
-    borderLeftColor: "#d32f2f"
+    borderLeftColor: "#d32f2f",
+    display: "flex"
   },
 
   media: {
@@ -30,16 +24,6 @@ const styleSheet = createStyleSheet("ErrorMessages", theme => ({
     display: "flex",
     alignSelf: "center",
     marginLeft: "10px"
-  },
-
-  main: {
-    display: "flex",
-    flexDirection: "row"
-  },
-
-  head: {
-    display: "flex",
-    flexFlow: "column wrap"
   },
 
   content: {
@@ -59,49 +43,15 @@ class ErrorMessages extends Component {
     return (
       <div className={classes.root}>
         <Card className={classes.card}>
-          <div className={classes.main}>
-            <CardMedia className={classes.media}>
-              <Error className={classes.icon} />
-            </CardMedia>
-            <div className={classes.head}>
-              <CardHeader title="We were unable to fetch data at this time" />
-              <CardContent className={classes.content}>
-                <Typography type="subheading">
-                  Please try again later
-                </Typography>
-              </CardContent>
-            </div>
+          <CardMedia className={classes.media}>
+            <Error className={classes.icon} />
+          </CardMedia>
+          <div>
+            <CardHeader title="We were unable to fetch your data at this time." />
+            <CardContent className={classes.content}>
+              <Typography type="subheading">Please try again later.</Typography>
+            </CardContent>
           </div>
-          <CardActions>
-            <Button
-              color="accent"
-              href="https://wwwp.oakland.edu/registrar/important-dates/#tab-5"
-              target="_blank"
-            >
-              Final Exam Dates
-            </Button>
-            <Button
-              color="accent"
-              href="https://wwwp.oakland.edu/gpa-calculator/"
-              target="_blank"
-            >
-              GPA Calculator
-            </Button>
-            <Button
-              color="accent"
-              href="https://wwwp.oakland.edu/oira/distribution-of-grades/"
-              target="_blank"
-            >
-              Grade Distributions
-            </Button>
-            <Button
-              color="accent"
-              href="https://sail.oakland.edu/PROD/bwskotrn.P_ViewTermTran"
-              target="_blank"
-            >
-              View Transcript
-            </Button>
-          </CardActions>
         </Card>
       </div>
     )
