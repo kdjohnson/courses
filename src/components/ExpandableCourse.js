0311101e795eba3194286083cab748b4e7ba7d89
KaJuan Johnson <kdjohnson23@oakland.edu>
2019-09-24 22:06:05 -0400

    Migrate ExpandableCourse to function, add usage of styles hook. Remove unused imports

--- src/components/ExpandableCourse.js
+++ src/components/ExpandableCourse.js
@@ -4,14 +4,12 @@ import CourseDetails from './CourseDetails'
 import CourseHeader from './CourseHeader'
 import Instructors from './Instructors'
 import Meetings from './Meetings'
-import PropTypes from 'prop-types'
-
 import Card from '@material-ui/core/Card'
 import CardActions from '@material-ui/core/CardActions'
 import CardContent from '@material-ui/core/CardContent'
-import { withStyles } from '@material-ui/core/styles'
+import { makeStyles } from '@material-ui/styles'
 
-const styles = theme => ({
+const useStyles = makeStyles(() => ({
   courseContainer: {
     flex: '1 1 auto',
     padding: '1em'
@@ -26,38 +24,32 @@ const styles = theme => ({
     display: 'flex',
     justifyContent: 'center'
   }
-})
+}))
 
-class ExpandableCourse extends React.Component {
-  render() {
-    const { classes, course, mobile } = this.props
-    return (
-      <div className={classes.courseContainer}>
-        <div style={{ marginTop: '1em' }}>
-          <Card className={classes.card}>
-            <CourseHeader mobile={mobile} course={course} />
-            <CardContent className={classes.content}>
-              <div>
-                <div style={{ marginTop: '1em' }}>
-                  <Meetings meetings={course.meetings} />
-                </div>
-              </div>
-            </CardContent>
-            <CardActions style={{ justifyContent: 'center', flexWrap: 'wrap' }}>
-              <CourseDetails course={course} />
-              <Instructors teachers={course.instructors} />
-            </CardActions>
-          </Card>
-        </div>
-      </div>
-    )
-  }
+const ExpandableCourse = props => {
+  const classes = useStyles()
+  const { course, mobile } = props
+
+  return (
+    <div className={classes.courseContainer}>
+    <div style={{ marginTop: '1em' }}>
+      <Card className={classes.card}>
+        <CourseHeader mobile={mobile} course={course} />
+        <CardContent className={classes.content}>
+          <div>
+            <div style={{ marginTop: '1em' }}>
+              <Meetings meetings={course.meetings} />
+            </div>
+          </div>
+        </CardContent>
+        <CardActions style={{ justifyContent: 'center', flexWrap: 'wrap' }}>
+          <CourseDetails course={course} />
+          <Instructors teachers={course.instructors} />
+        </CardActions>
+      </Card>
+    </div>
+  </div>  
+  )
 }
 
-ExpandableCourse.propTypes = {
-  classes: PropTypes.object.isRequired
-}
-
-export default withStyles(styles, { name: 'ExpandableCourse' })(
-  ExpandableCourse
-)
+export default ExpandableCourse
