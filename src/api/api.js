99e6a1ee4c89baf2c7299e76115e84a1aa2ecf20
Samantha Bracellari <sbracellari@oakland.edu>
2020-08-04 08:37:08 -0400

    replaces local backend with demo data #209

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,35 @@
 /* global token */
 
-export const get_courses = async (term, url) => {
+import courses from './courses.json'
+import events from './events.json'
+
+export const get_courses = async (is_demo, term, url) => {
+  if (is_demo) {
+    return courses
+  }
+
+  try {
+    const response = await fetch(url + term, {
+      credentials: 'include',
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        Authorization: 'Bearer ' + token 
+      }
+    })
+
+    const data = await response.json()
+    return data
+  } catch (err) {
+    return err
+  }
+}
+
+export const get_events = async (is_demo, term, url) => {
+  if (is_demo) {
+    return events
+  }
+  
   try {
     const response = await fetch(url + term, {
       credentials: 'include',
